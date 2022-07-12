e87e17ca55b8e70e412cb9760682141df394894e
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-12 20:09:56 -0300

    H2 database, test profile, JPA

--- src/main/java/com/app/course/CourseApplication.java
+++ src/main/java/com/app/course/CourseApplication.java
@@ -2,7 +2,9 @@ package com.app.course;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan(basePackages = "com.app.course.entities")
 @SpringBootApplication
 public class CourseApplication {
 
--- src/main/java/com/app/course/resources/UserResource.java
+++ src/main/java/com/app/course/resources/UserResource.java
@@ -5,15 +5,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.app.course.entities.User;
+import com.app.course.entities.Usuario;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "12345");
+	public ResponseEntity<Usuario> findAll() {
+		Usuario u = new Usuario(1L, "Maria", "maria@gmail.com", "9999999", "12345");
 		return ResponseEntity.ok().body(u);
 	}
 }
