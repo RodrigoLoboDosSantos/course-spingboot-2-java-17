b45c479c9e0628f8748b04a70f2b7a5f728933f2
Rodrigo Lobo dos Santos <lobo_773@hotmail.com>
2022-07-15 19:01:09 -0300

    Payment, one-to-one association

--- src/main/java/com/app/course/config/TestConfig.java
+++ src/main/java/com/app/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.app.course.entities.Category;
 import com.app.course.entities.Order;
 import com.app.course.entities.OrderItem;
+import com.app.course.entities.Payment;
 import com.app.course.entities.Product;
 import com.app.course.entities.Usuario;
 import com.app.course.entities.enums.OrderStatus;
@@ -81,6 +82,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	
 	}
 }
